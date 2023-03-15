f817167011684b97b6e05bf9d41e36a6eaab5813
vora137 <vora137@naver.com>
2023-03-15 11:48:53 +0900

    0315수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useState, useEffect } from 'react';
-import { StatusBar, Dimensions, Alert } from 'react-native';
+import { StatusBar, Dimensions, Alert, Pressable, Text } from 'react-native';
 import styled, { ThemeProvider } from 'styled-components/native';
 import { theme } from './theme';
 import Input from './components/Input';
@@ -15,17 +15,35 @@ const Container = styled.SafeAreaView`
 `;
 
 const Title = styled.Text`
-  font-size: 40px;
+  font-size: 30px;
   font-weight: 600;
   color: ${({ theme }) => theme.main};
   align-self: center;
-  margin: 0px 20px;
+  letter-spacing: 10px;
+  margin: 0px 20px; /* y축 x축 */
 `;
 
 const List = styled.ScrollView`
   flex: 1;
   width: ${({ width }) => width - 40}px;
 `;
+const AllItemDelBtn = ({ width, onPress, title }) => {
+  return (
+    <Pressable
+      style={{
+        backgroundColor: '#666',
+        width: width - 40,
+        margin: 5,
+        borderRadius: 10, // 객체리터럴 > 단위 안붙여도됨
+      }}
+      onPress={onPress}
+    >
+      <Text style={{ textAlign: 'center', padding: 5, color: '#fff' }}>
+        {title}
+      </Text>
+    </Pressable>
+  );
+};
 
 SplashScreen.preventAutoHideAsync();
 
@@ -35,10 +53,14 @@ const App = () => {
   const [isReady, setIsReady] = useState(false);
   const [newTask, setNewTask] = useState('');
   const [tasks, setTasks] = useState({});
+  const [completedCnt, setCompletedCnt] = useState(0); // 상태변수를 만드는 함수 .
 
   // 데이터 저장
   const _saveTasks = async tasks => {
     try {
+      setCompletedCnt(
+        Object.values(tasks).filter(item => item.completed).length, //완료건수
+      );
       await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
       setTasks(tasks);
     } catch (e) {
@@ -48,8 +70,13 @@ const App = () => {
   // 데이터 불러오기
   const _loadTasks = async () => {
     try {
-      const loadedTasks = await AsyncStorage.getItem('tasks');
-      setTasks(JSON.parse(loadedTasks || '{}'));
+      let loadedTasks = await AsyncStorage.getItem('tasks');
+      loadedTasks = JSON.parse(loadedTasks) || {};
+      setTasks(loadedTasks);
+      // 초기에도 완료항목 카운트 되게 지정
+      setCompletedCnt(
+        Object.values(loadedTasks).filter(item => item.completed).length,
+      );
     } catch (e) {
       console.error(e);
     }
@@ -67,6 +94,7 @@ const App = () => {
     }
     prepare();
   }, []);
+
   const onLayoutRootView = useCallback(async () => {
     if (isReady) {
       await SplashScreen.hideAsync();
@@ -90,9 +118,24 @@ const App = () => {
 
   // 삭제
   const _deleteTask = id => {
-    const currentTasks = Object.assign({}, tasks);
-    delete currentTasks[id];
-    _saveTasks(currentTasks);
+    const currentTasks = { ...tasks }; //Object.assign({},tasks);
+
+    Alert.alert('', '삭제하시겠습니까?', [
+      {
+        text: '아니오',
+        onPress() {
+          console.log('아니오');
+        },
+      },
+      {
+        text: '예',
+        onPress() {
+          console.log('예');
+          delete currentTasks[id];
+          _saveTasks(currentTasks);
+        },
+      },
+    ]);
   };
 
   // 완료
@@ -111,12 +154,41 @@ const App = () => {
 
   const _handleTextChange = text => {
     setNewTask(text);
+    console.log(`변경된문자열:${newTask}`);
   };
 
   // 입력취소
   const _onBlur = () => {
     setNewTask('');
   };
+
+  // 전체삭제
+  const _delAll = () => {
+    const currentTasks = { ...tasks };
+    if (Object.values(currentTasks).filter(item => item.completed).length < 1) {
+      Alert.alert('', '완료 항목이 없습니다', [{ text: '확인' }]);
+      return;
+    }
+
+    Alert.alert(
+      '[주의]',
+      `완료항목 ${completedCnt}건 있습니다. \n 전체삭제 하시겠습니까?`,
+      [
+        { text: '아니오', onPress: () => {} },
+        {
+          text: '예',
+          onPress() {
+            const currentTasks = { ...tasks };
+            Object.keys(currentTasks)
+              .filter(key => currentTasks[key].completed)
+              .forEach(key => delete currentTasks[key]);
+            _saveTasks(currentTasks);
+          },
+        },
+      ],
+    );
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Container onLayout={onLayoutRootView}>
@@ -145,6 +217,13 @@ const App = () => {
               />
             ))}
         </List>
+        <AllItemDelBtn
+          onPress={() => {
+            _delAll();
+          }}
+          title={`완료항목(${completedCnt})건 전체 삭제`}
+          width={width}
+        />
       </Container>
     </ThemeProvider>
   );
